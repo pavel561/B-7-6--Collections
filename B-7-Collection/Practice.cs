a2b731e7a0bae36e3d7ad975c1b42511ac315407
PavelGornostaev <pavel561@gmail.com>
2019-01-04 14:33:40 +0300

    Добавьте файлы проекта.

---
+++ B-7-Collection/Practice.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base.Lesson_5
+{
+	class Practice
+	{
+		/*public interface IComparable
+		{
+			int CompareTo(object o);
+		}
+		public class poemArray
+		{
+			public string Text { get; set; }
+		}
+		*/
+		public static void B7_P1_5_ArrayListPoemSort()
+		{
+			
+			ArrayList arrayList = new ArrayList();
+			for (int i = 5; i > 0; i--)
+			{
+				Console.WriteLine($"Введите строку стихотворения. Осталось ввести строк - {i}");
+				Console.Write(" >> ");
+				arrayList.Add(Console.ReadLine());
+			}
+			Console.WriteLine("= After arrayList.Add ==========================");
+			foreach (var str in arrayList )
+			{
+				Console.WriteLine(str);
+			}
+			Console.WriteLine("= After arrayList.Sort ==========================");
+			arrayList.Sort();
+			foreach (var str in arrayList)
+			{
+				Console.WriteLine(str);
+			}
+			Console.WriteLine("= After arrayList.RemoveAt ==========================");
+			arrayList.RemoveAt(arrayList.Count - 1);
+			foreach (var str in arrayList)
+			{
+				Console.WriteLine(str);
+			}
+			//var arraySorted = from str in arrayList orderby arrayList.
+			Console.ReadLine();
+		}
+
+		public class Song
+		{
+			public string Name;
+			public string Lyrics { get; set; }
+			public Song()
+			{
+				Lyrics = "";
+			}
+			public Song(string lyrics)
+			{
+				Lyrics = lyrics;
+			}
+		}
+		static public void B7_P2_5_ArrayListOfSongsSort()
+		{
+
+			ArrayList songList = new ArrayList();
+			for (int i = 5; i > 0; i--)
+			{
+				Console.WriteLine($"Введите текст добавляемой песни. Осталось добавить песен - {i}");
+				Console.Write(" >> ");
+				songList.Add(new Song(Console.ReadLine()));
+			}
+			Console.WriteLine("= After arrayList.Add ==========================");
+			foreach (var song in songList)
+			{
+				Console.WriteLine(((Song)song).Lyrics);
+			}
+			/*
+			Console.WriteLine("= After arrayList.Sort ==========================");
+			songList.Sort();
+			foreach (var song in songList)
+			{
+				Console.WriteLine(((Song)song).Lyrics);
+			}
+			*/
+			Console.WriteLine("= After arrayList.RemoveAt ==========================");
+			songList.RemoveAt(songList.Count - 1);
+			foreach (var song in songList)
+			{
+				Console.WriteLine(((Song)song).Lyrics);		//Явное указание типа объекта
+			}
+			//var arraySorted = from str in arrayList orderby arrayList.
+			Console.ReadLine();
+		}
+	}
+}
---
+++ B-7-Collection/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base.Lesson_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			//Practice.B7_P1_5_ArrayListPoemSort();
+			Practice.B7_P2_5_ArrayListOfSongsSort();
+			//ArrayListExample();
+			//ArrayListExample2();
+
+			Console.ReadLine();
+        }
+
+		static void ArrayListExample2()
+		{
+			var list = new ArrayList();
+			for(int i = 0 ; i < 3; i++)
+			{
+				Console.WriteLine("Next row please");
+				//var song = new ;
+				list.Add(Console.ReadLine());
+			}
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine();
+			list.Sort();
+
+			list.RemoveAt(list.Count - 1);
+			foreach(var row in list)
+			{
+				Console.WriteLine(row);
+			}
+		}
+		/*
+		public class Song : //IComparable
+		{
+			public string Lyrics { get; set; }
+			
+		}
+		*/
+		/*
+        public static void ArrayListExample()
+        {
+            var poem = new ArrayList();
+            for (int i = 0; i < 5; i++)
+            {
+                var song = new Song();
+                song.Lyrics = Console.ReadLine();
+                poem.Add(song);
+            }
+
+            //poem.Sort();
+            poem.RemoveAt(poem.Count - 1);
+
+            object[] myArray = poem.ToArray();
+
+            foreach (var item in myArray)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+		*/
+		/*
+		public class Song
+        {
+            public string Lyrics;
+
+            public override string ToString()
+            {
+                return this.Lyrics;
+            }
+        }
+		*/
+    }
+}
